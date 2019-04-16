c92f3badbfd85e4713bd64d6755d00c55a501067
BCIceman <bciceman@bctech.net>
2019-04-16 04:50:24 -0400

    s

--- cbomatic/runtime.pl
+++ cbomatic/runtime.pl
@@ -180,8 +180,9 @@ ctlc(); usleep 100000; ctl9(); usleep 200000; ctlc(); usleep 100000; ctl9(); usl
 sub sensor1 {
  while () {
   my $sensor1raw = "WTF";
-  if ($sensortype == 2) { chomp($sensor1raw = `python /home/iceman/Adafruit_Python_DHT/examples/AdafruitDHT.py 22 4`); } elsif ($sensortype == 3) { chomp($sensor1raw = `/usr/bin/bme280`); } else { $sensor1raw = ''; };
+  if ($sensortype == 2) { print "sensor2\n"; chomp($sensor1raw = `python /home/iceman/Adafruit_Python_DHT/examples/AdafruitDHT.py 22 4`); } elsif ($sensortype == 3) { chomp($sensor1raw = `/usr/bin/bme280`); } else { $sensor1raw = ''; };
   $handle->shlock();
+  print "setbuf\n";
   $buffer = $sensor1raw;
   $handle->shunlock();
   sleep 4;
