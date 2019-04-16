06ce26895602658784b9376bdb2ccefe4c30e098
BCIceman <bciceman@bctech.net>
2019-04-16 04:05:34 -0400

    d

--- cbomatic/runtime.pl
+++ cbomatic/runtime.pl
@@ -200,6 +200,7 @@ sub tempf {
  my $sensor1humid;
  my $sensor1tempc;
  my $sensor1raw = $buffer;
+  $sensor1raw ||= "FuckingFail";
  print "raw:" . $sensor1raw . " :end\n";
  if ($sensortype == 2) { 
  my $sensor1regex = '^[^0-9]*([0-9\\.]+)[^0-9]*([0-9\\.]+)[^0-9]*$';
